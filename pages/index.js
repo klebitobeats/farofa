df94ff41be17e1b1e8d271977877cd1e6b2fc410
klebitobeats <klebsonclesio@gmail.com>
2025-07-08 14:14:59 -0300

    primeiro comit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-purple-100">
+      <h1 className="text-4xl font-bold text-purple-700">Olá, Tailwind está funcionando!</h1>
+    </div>
+  );
+}
